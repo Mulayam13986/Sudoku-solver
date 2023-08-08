6d0861f1da7f56764c2c5262f702a0a782c3c73e
Mulayam <mulyam13986@gmail.com>
2023-08-09 03:06:51 +0530

    function updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function startGame() {
     remainingTime = interval;
     updateTimer();
 
-    countdown = setInterval(function() {
+    countdown = setInterval(function () {
         remainingTime--;
         if (remainingTime <= 0) {
             clearInterval(countdown);
@@ -26,35 +26,130 @@ function startGame() {
         }
     }, 1000);
 }
-const cells = document.querySelectorAll(".cell")
-
-cells.forEach(cell => {
-    cell.addEventListener('input', () => {
-        const value = Number(cell.innerText);
-        if (Number.isNaN(value) || value < 1 || value > 9) {
-            cell.innerText = '';
-        }
-    });
-});
-
-
 
 
 // start function
 
 let isGame = 0;
-let board 
-const gridCells = document.querySelectorAll('.cell');
-console.log(gridCells);
 
-// Define an empty 9x9 grid
-const emptyGrid = Array.from({ length: 9 }, () => Array(9).fill(0));
+const gridCells = document.querySelectorAll('.cell');
+// console.log(gridCells);
+
+// initialising board 
+let board = [];
+for (let i = 0; i < 9; i++) {
+    let row = [];
+    for (let j = 0; j < 9; j++) {
+        row.push(0);
+    }
+    board.push(row);
+}
+
+let count = 0;
+
+const cells = document.querySelectorAll(".cell");
+for (let cell of gridCells) {
+
+    // console.log(cell.innerText);
+
+    let i = parseInt(count / 9);
+    // console.log(count/9);
+    let j = count % 9;
+    count += 1;
+    board[i][j] = cell.innerText;
+
+}
+
+cells.forEach(cell => {
+    cell.addEventListener('input', () => {
+        const value = Number(cell.innerText);
+        if (Number.isNaN(value) || value < 1 || value > 9) {
+            cell.innerText = "";
+        }
+        else {
+        let id_cell = parseInt(cell.getAttribute("id"));
+        console.log(id_cell);
+        let i = parseInt(id_cell / 9);
+        let j = id_cell % 9;
+        board[i][j] = value;
+        }
+        
+    });
+});
+
+function boardConstruction(gridCells) {
+    let board = [];
+    for (let i = 0; i < 9; i++) {
+        let row = [];
+        for (let j = 0; j < 9; j++) {
+            row.push(0);
+        }
+        board.push(row);
+    }
+
+
+    let count = 0;
+    for (let cell of gridCells) {
+
+        // console.log(cell.innerText);
+
+        let i = parseInt(count / 9);
+        // console.log(count/9);
+        let j = count % 9;
+        count += 1;
+        board[i][j] = cell.innerText;
+
+    }
+
+    return board ;
+
+}
+
+
+// function which will give alert when user submits the puzzle if its true or false;
+
+function submit() {
+
+    
+    let rows = new Set();
+    let cols = new Set();
+    let boxes = new Set();
+    let curRowElem;
+    let curColElem;
+    let curBoxElem;
+
+    for (let i = 0; i < board.length; i += 1) {
+        for (let j = 0; j < board[0].length; j += 1) {
+            curRowElem = board[i][j]
+            curColElem = board[j][i]
+            curBoxElem = board[3 * Math.floor(i / 3) + Math.floor(j / 3)][((i * 3) % 9) + (j % 3)]
+
+            if (curRowElem == "" || curColElem == "" || curColElem == "") {
+                document.getElementById("result").innerHTML = `<h2 class="text-danger" id="result">Better Luck Next Time </h2>`
+                return false;
+            }
+            if (rows.has(curRowElem) || cols.has(curColElem) || (boxes.has(curBoxElem))) {
+                document.getElementById("result").innerHTML = `<h2 class="text-danger" id="result">Better Luck Next Time </h2>`
+                return false;
+            }
+            rows.add(curRowElem);
+            cols.add(curColElem);
+            boxes.add(curBoxElem);
+        }
+
+        rows.clear()
+        cols.clear()
+        boxes.clear()
+    }
+    document.getElementById("result").innerHTML = `<h2 class="text-success" id="result">You have successfully solved the sudoku</h2>`
+    return true;
+}
 
 // Function to check if a number is valid at a given position
 function isValid(board, row, col, num) {
     // Check row and column
     for (let i = 0; i < 9; i++) {
-        if (board[row][i] === num || board[i][col] === num) {
+        if (board[row][i] == String(num) || board[i][col] == String(num)) {
             return false;
         }
     }
@@ -64,7 +159,7 @@ function isValid(board, row, col, num) {
     const startCol = Math.floor(col / 3) * 3;
     for (let i = startRow; i < startRow + 3; i++) {
         for (let j = startCol; j < startCol + 3; j++) {
-            if (board[i][j] === num) {
+            if (board[i][j] == String(num)) {
                 return false;
             }
         }
@@ -73,83 +168,63 @@ function isValid(board, row, col, num) {
     return true;
 }
 
-// function for submit
-
-// function submit(){
-
-//     let flag = 1;
-//     // checking all rows
-//     for (let i = 0 ; i < 9 ;i++){
-//         // checking row i 
-
-//         for(let j = 0 ; j < 9 ; j++){
-//             // checking element j of row i 
-
-//             for(let k = 0 ; k < 9 ; k++){
-
-                
-//             }
-            
-//         }
-//     }
-
-
-// }
-
-// function for answer 
-
-function answer(){
-
-    dfs(board, 9);
+function answer() {
+    console.log(board);
+    if (dfs(board, 9)) {
+        console.log("solution is possible");
+    }
+    else {
+        console.log("solution not possible");
+    }
+    fillValues(board);
 }
 
 
 
-  function dfs(board, n) {
+function dfs(board, n) {
     // for every cell in the sudoku
     for (let row = 0; row < n; row++) {
-      for (let col = 0; col < n; col++) {
-        // if its empty
-        if (board[row][col] !== '.') continue;
-        // try every number 1-9
-        for (let i = 1; i <= 9; i++) {
-          const c = i;
-          // if that number is valid
-          if (isValid(board, row, col, c)) {
-            board[row][col] = c;
-            // continue search for that board, ret true if solution is reached
-            if (dfs(board, n)) return true;
-          }
+        for (let col = 0; col < n; col++) {
+            // if its empty
+            if (board[row][col] !== "") continue;
+            // try every number 1-9
+            for (let i = 1; i <= 9; i++) {
+                const c = i;
+                // if that number is valid
+                if (isValid(board, row, col, c)) {
+                    board[row][col] = String(c);
+                    // continue search for that board, ret true if solution is reached
+                    if (dfs(board, n)) {
+                        return true;
+                    }
+                    else {
+                        board[row][col] = "";
+                    }
+                }
+            }
+            // solution wasnt found for any num 1-9 here, must be a dead end...
+
+            // ret false to signal dead end 
+            return false;
         }
-        // solution wasnt found for any num 1-9 here, must be a dead end...
-        // set the current cell back to empty
-        board[row][col] = '.';
-        // ret false to signal dead end 
-        return false;
-      }
     }
     // all cells filled, must be a solution
     return true;
-  }
-  
-// Backtracking solver
-// Function to initialize and solve the puzzle
-function startSolver() {
-    const level = document.querySelector('input[name="level"]:checked').value;
-    const puzzle = generatePuzzle(level);
-    const gridCells = document.querySelectorAll('.sudoku-grid .cell');
+}
 
-    for (let row = 0; row < 9; row++) {
-        for (let col = 0; col < 9; col++) {
-            const index = row * 9 + col;
-            if (puzzle[row][col] !== 0) {
-                gridCells[index].textContent = puzzle[row][col];
-            } else {
-                gridCells[index].textContent = '';
-            }
+function fillValues() {
+
+    for (let i = 0; i < 9; i++) {
+
+        for (let j = 0; j < 9; j++) {
+
+            let id_cell = String(9 * i + j);
+
+            document.getElementById(id_cell).innerText = board[i][j];
         }
     }
 }
+
 
 // Function to generate a Sudoku puzzle of a specified level
 function generatePuzzle(level) {
@@ -159,9 +234,20 @@ function generatePuzzle(level) {
 }
 
 // Function to clear the grid
-function resetGrid() {
-    const gridCells = document.querySelectorAll('.sudoku-grid .cell');
-    gridCells.forEach(cell => {
-        cell.textContent = '';
-    });
+function restart() {
+
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            board[i][j] = '';
+        }
+    }
+    fillValues(board);
+}
+
+
+// generate board : 
+
+function generate(){
+
+
 }
