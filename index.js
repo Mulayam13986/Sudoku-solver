ac2c86e294e285d0cbd9a5deb48dd1f04c95c72e
Mulayam <mulyam13986@gmail.com>
2023-08-11 15:08:09 +0530

    Final Changes

--- index.js
+++ index.js
@@ -4,27 +4,39 @@ let interval;
 let remainingTime = 0;
 
 
+
 function updateTimer() {
+
+    
     const timerElement = document.getElementById('timer');
     const minutes = Math.floor(remainingTime / 60).toString().padStart(2, '0');
     const seconds = (remainingTime % 60).toString().padStart(2, '0');
     timerElement.textContent = `${minutes}:${seconds}`;
 }
-
+var already_start = false;
 function start() {
+    if (already_start === true) {
+        clearInterval(countdown);
+    }
+    already_start = true;
     const intervalSelect = document.getElementById('interval');
     let time_interval = document.querySelector("input[name='time']:checked").value;
     interval = parseInt(time_interval, 10) * 60;
 
     remainingTime = interval;
+    
     updateTimer();
-
+    // console.log(interval);
     countdown = setInterval(function () {
         remainingTime--;
+        if (remainingTime < interval/10 ){
+            document.getElementById("timer").style.color = "red";
+        }
         if (remainingTime < 0) {
             clearInterval(countdown);
             document.getElementById("result").innerHTML = `<h2 class="text-success" id="result">Time's Up</h2>`
-        } else {
+        } 
+        else {
             updateTimer();
         }
     }, 1000);
@@ -65,7 +77,7 @@ for (let cell of gridCells) {
 
 cells.forEach(cell => {
     cell.addEventListener('input', () => {
-        
+
         const value = Number(cell.innerText);
 
         let id_cell = parseInt(cell.getAttribute("id"));
@@ -73,15 +85,15 @@ cells.forEach(cell => {
         let j = id_cell % 9;
 
         if (Number.isNaN(value) || value < 1 || value > 9) {
-            
+
             board[i][j] = "";
             cell.innerText = "";
         }
         else {
-        board[i][j] = String(value);
-        document.getElementById(id_cell).style.color = "red";
+            board[i][j] = String(value);
+            document.getElementById(id_cell).style.color = "red";
         }
-        
+
     });
 });
 
@@ -109,7 +121,7 @@ function boardConstruction(gridCells) {
 
     }
 
-    return board ;
+    return board;
 
 }
 
@@ -118,8 +130,11 @@ function boardConstruction(gridCells) {
 
 function submit() {
 
-    console.log(board);
-    
+    clearInterval(countdown);
+    remainingTime = 0;
+    updateTimer();
+    // console.log(board);
+
     let rows = new Set();
     let cols = new Set();
     let boxes = new Set();
@@ -150,6 +165,7 @@ function submit() {
         cols.clear()
         boxes.clear()
     }
+
     document.getElementById("result").innerHTML = `<h2 class="text-success" id="result">You have successfully solved the sudoku</h2>`
     return true;
 }
@@ -178,12 +194,17 @@ function isValid(board, row, col, num) {
 }
 
 function answer() {
-    console.log(board);
+    clearInterval(countdown);
+    remainingTime = 0;
+    updateTimer();
+    // console.log(board);
+
     if (dfs(board, 9)) {
-        console.log("solution is possible");
+        // console.log("solution is possible");
     }
     else {
-        console.log("solution not possible");
+        // console.log("solution not possible");
+
     }
     fillValues(board);
 }
@@ -238,11 +259,13 @@ function fillValues() {
 
 // Function to clear the grid
 function restart() {
-    
+    clearInterval(countdown);
+    remainingTime = 0;
+    updateTimer();
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
             board[i][j] = '';
-            id_cell = String(i*9+j);
+            id_cell = String(i * 9 + j);
             document.getElementById(id_cell).style.color = "#333333";
         }
     }
@@ -253,7 +276,7 @@ function restart() {
 
 // generate board : 
 
-function generate(){
+function generate() {
 
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
@@ -261,18 +284,18 @@ function generate(){
             // document.getElementById(String(8*i+j)).contentEditable = false;
         }
     }
-    document.getElementsByClassName("cell").contentEditable= false;
-    console.log(board);
-    help_to_generate(board,9);
-    console.log(board);
+    document.getElementsByClassName("cell").contentEditable = false;
+    // console.log(board);
+    help_to_generate(board, 9);
+    // console.log(board);
     empty_cell();
     fillValues();
-    
+
 
 }
 
 
-function help_to_generate(board,n){
+function help_to_generate(board, n) {
 
     // for every cell in the sudoku
     for (let row = 0; row < n; row++) {
@@ -280,20 +303,20 @@ function help_to_generate(board,n){
             // if its empty
             if (board[row][col] !== "") continue;
             // try every number 1-9
-            let vis  =[0,0,0,0,0,0,0,0,0,0];
-            while(true) {
-                const c = Math.floor(Math.random()*9 +1 );
+            let vis = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            while (true) {
+                const c = Math.floor(Math.random() * 9 + 1);
                 let flag = 1;
 
-                for (let idx = 1 ; idx <= 9 ;idx++){
+                for (let idx = 1; idx <= 9; idx++) {
 
-                    if (vis[idx] == 0){
+                    if (vis[idx] == 0) {
                         flag = 0;
                         break;
                     }
                 }
-                if(flag) break;
-                if(vis[c] == 1) continue;
+                if (flag) break;
+                if (vis[c] == 1) continue;
                 vis[c] = 1;
                 // if that number is valid
                 if (isValid(board, row, col, c)) {
@@ -317,43 +340,43 @@ function help_to_generate(board,n){
     return true;
 }
 
-function empty_cell(){
+function empty_cell() {
 
 
-    let count = 0 ;
+    let count = 0;
 
-    let k = Math.floor(Math.random()*5+1);
+    let k = Math.floor(Math.random() * 5 + 1);
 
     let var1 = document.querySelector("input[name='level']:checked").id;
 
-    if(var1 == "beginner"){
-        k+=16;
+    if (var1 == "beginner") {
+        k += 16;
     }
-    else if (var1 == "easy"){
-        k+= 25;
+    else if (var1 == "easy") {
+        k += 25;
     }
-    else if (var1 == "medium"){
-        k+=39;
+    else if (var1 == "medium") {
+        k += 39;
     }
-    else{
-        k+=55;
+    else {
+        k += 55;
     }
-  
 
-    while(count < k ){
 
-        let i = Math.floor(Math.random()*9);
-        let j = Math.floor(Math.random()*9 ) ;
+    while (count < k) {
+
+        let i = Math.floor(Math.random() * 9);
+        let j = Math.floor(Math.random() * 9);
 
         if (board[i][j] == "") continue;
         else {
 
             board[i][j] = "";
-            let id_empty_cell = String(9*i+j);
+            let id_empty_cell = String(9 * i + j);
             document.getElementById(id_empty_cell).style.color = "red";
-            document.getElementById(id_empty_cell).contentEditable= true;
-            
-            count++; 
+            document.getElementById(id_empty_cell).contentEditable = true;
+
+            count++;
         }
     }
 
